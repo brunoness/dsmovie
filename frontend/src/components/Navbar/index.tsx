396b5a047fe167be983741ef6d6c73e765817ca9
Bruno Correia <bruno.h.c@hotmail.com>
2022-01-10 21:12:02 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>Vídeo Locadora</h1>
+                    <a href="https://github.com/brunoness" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/brunoness</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
