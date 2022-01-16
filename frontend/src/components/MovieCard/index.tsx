9239fcb8484a81501d8e787f462067e599e45d86
Bruno Correia <bruno.h.c@hotmail.com>
2022-01-16 01:24:30 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,7 +1,6 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
-import "./styles.css";
 
 type Props = {
     movie: Movie;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -23,7 +23,7 @@ function Listing() {
 
   useEffect(() => {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
       .then(response => {
         const data = response.data as MoviePage;
         setPage(data);
@@ -37,19 +37,19 @@ function Listing() {
 
       <div className="container">
         <div className="row">
-          {page.content.map(movie =>
+          {page.content.map(movie => (
             <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3">
               <MovieCard movie={movie} />
             </div>
           )
-          }
+          )}
         </div>
       </div>
 
 
     </>
 
-  )
+  );
 }
 
 export default Listing;
